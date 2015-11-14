45e2e78ecb9cd3cef69b693cfb75a19484462519
Jon <jon.se5a@gmail.com>
2015-11-14 18:45:48 +1300

    some files did not get added to git

---
+++ Pulsar4X/Pulsar4X.Networking/Base.cs
@@ -0,0 +1,243 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+
+using System.IO;
+
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+using Lidgren.Network;
+using Pulsar4X.ECSLib;
+
+
+
+namespace Pulsar4X.Networking
+{
+
+
+    public enum DataMessageType : byte
+    {
+        StringMessage,
+        FactionDictionary,
+        GameData,
+        EntityData,
+        DataBlobData,
+        DataBlobPropertyUpdate,
+        FactionDataRequest,
+
+        TickInfo
+    }
+
+    /*Messages look like:
+     * 
+     * GameData, (string)gameName, (long)currentDate
+     * EntityData, (Byte[])Guid, (Byte[])memeoryStream
+     * 
+     * 
+     * FactionDataRequest, (string)factionName, (string)password
+     * TickInfo, (long)fromDate, (long)Delta
+     * 
+     */
+
+    //[Serializable]
+    //public class DataMessage
+    //{
+    //    public DataMessageType DataMessageType { get; set; }
+    //    public object DataObject { get; set; }
+
+    //    public Guid EntityGuid { get; set; }
+    //    public string PropertyName { get; set; }//actualy can I look at how wpf does this?
+    //}
+
+    /// <summary>
+    /// a short struct which holds string name and guid of a faction. 
+    /// </summary>
+    [Serializable]
+    public struct FactionItem
+    {
+        public string Name { get; set; }
+        public Guid ID { get; set; }
+    }
+
+    public class NetworkBase
+    {
+        public Game Game { get; set; }
+        //protected GameVM _gameVM_ { get; set; }
+        protected const int SecureChannel = 31;
+        private readonly ObservableCollection<string> _messages;
+        public ObservableCollection<string> Messages { get { return _messages; } }
+
+        public int PortNum { get; set; }
+        public NetPeer NetPeerObject { get; set; }
+
+        public NetworkBase()
+        {
+            _messages = new ObservableCollection<string>();
+            _messages.Add("Start of Messages");
+        }
+
+        protected void StartListning()
+        {
+            NetPeerObject.RegisterReceivedCallback(new SendOrPostCallback(GotMessage));
+        }
+
+        /// <summary>
+        /// This gets called when triggered by an event regestered in StartListning()
+        /// </summary>
+        /// <param name="peer">this is NetPeerObject so just using that</param>
+        public void GotMessage(object peer)
+        {
+            NetIncomingMessage message;
+
+            while ((message = NetPeerObject.ReadMessage()) != null)
+            {
+                if (message.SequenceChannel == SecureChannel)
+                {
+                    DecryptedReceve(message);
+                }
+                switch (message.MessageType)
+                {
+                    case NetIncomingMessageType.Data:
+                        // handle custom messages
+                        Messages.Add("Data Message from: " + message.SenderConnection.RemoteUniqueIdentifier);
+                        HandleIncomingDataMessage(message.SenderConnection, message);
+                        break;
+
+                    case NetIncomingMessageType.DiscoveryRequest:
+                        HandleDiscoveryRequest(message);
+                        break;
+                    case NetIncomingMessageType.DiscoveryResponse:
+                        HandleDiscoveryResponce(message);
+                        break;
+
+                    case NetIncomingMessageType.StatusChanged:
+                        // handle connection status messages
+                        Messages.Add("New status: " + message.SenderConnection.Status + " (Reason: " + message.ReadString() + ")");
+                        ConnectionStatusChanged(message);
+                        break;
+                    case NetIncomingMessageType.DebugMessage:
+                        // handle debug messages
+                        // (only received when compiled in DEBUG mode)
+                        Messages.Add("Debug Msg: " + message.ReadString());
+                        break;
+
+                    /* .. */
+                    default:
+                        Messages.Add(("unhandled message with type: " + message.MessageType));
+                        break;
+                }
+            }
+        }
+
+        /// <summary>
+        /// TODO implement proper private/public key. is that even possible to do transperantly?
+        /// </summary>
+        /// <param name="recever"></param>
+        /// <param name="message"></param>
+        protected NetOutgoingMessage Encrypt(NetOutgoingMessage message)
+        {
+            NetEncryption algo = new NetXtea(NetPeerObject, "SharedKey45B635DF-649B-4C10-B110-439CE1784C59");
+            message.Encrypt(algo);
+            return message;
+        }
+
+
+        protected NetIncomingMessage DecryptedReceve(NetIncomingMessage message)
+        {
+            NetEncryption algo = new NetXtea(NetPeerObject, "SharedKey45B635DF-649B-4C10-B110-439CE1784C59");
+            message.Decrypt(algo);
+            return message;
+        }
+
+        protected virtual void HandleDiscoveryRequest(NetIncomingMessage message)
+        {
+        }
+        protected virtual void HandleDiscoveryResponce(NetIncomingMessage message)
+        {
+        }
+        protected virtual void ConnectionStatusChanged(NetIncomingMessage message)
+        {
+        }
+        protected void HandleIncomingDataMessage(NetConnection sender, NetIncomingMessage message)
+        {
+            DataMessageType messageType = (DataMessageType)message.ReadByte();
+            switch (messageType)
+            {
+                case DataMessageType.FactionDataRequest:
+                    HandleFactionDataRequest(message);
+                    break;
+                case DataMessageType.GameData:
+                    HandleGameDataMessage(message);
+                    break;
+                case DataMessageType.TickInfo:
+                    HandleTickInfo(message);
+                    break;
+                case DataMessageType.EntityData:
+                    HandleEntityData(message);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// server only
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="message"></param>
+        protected virtual void HandleFactionDataRequest(NetIncomingMessage message)
+        {
+        }
+
+
+        /// <summary>
+        /// this one should be client only
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="message"></param>
+        protected virtual void HandleEntityData(NetIncomingMessage message)
+        {
+        }
+
+        /// <summary>
+        /// client only
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="message"></param>
+        protected virtual void HandleTickInfo(NetIncomingMessage message)
+        {
+        }
+
+
+
+        /// <summary>
+        /// client only
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="message"></param>
+        protected virtual void HandleGameDataMessage(NetIncomingMessage message)
+        {
+        }
+
+
+        /// <summary>
+        /// use this for sending an non basic object type.
+        /// </summary>
+        /// <param name="dataMessage"></param>
+        /// <returns></returns>
+        //protected NetOutgoingMessage SerialiseDataMessage(DataMessage dataMessage)
+        //{
+        //    //turn the dataMessage into a stream.
+        //    var binFormatter = new BinaryFormatter();
+        //    var mStream = new MemoryStream();
+        //    binFormatter.Serialize(mStream, dataMessage);
+        //    NetOutgoingMessage sendMsg = NetPeerObject.CreateMessage();
+        //    sendMsg.Write(mStream.ToArray()); //send the stream as an byte array. 
+        //    return sendMsg;
+        //}
+
+        //protected void HandleIncomingStringMessage(NetConnection sender, DataMessage dataMessage)
+        //{
+        //    Messages.Add(sender.RemoteUniqueIdentifier + " Sent: " + (string)dataMessage.DataObject);
+        //}
+
+    }
+}
---
+++ Pulsar4X/Pulsar4X.Networking/Client.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using Lidgren.Network;
+using Pulsar4X.ECSLib;
+
+namespace Pulsar4X.Networking
+{
+    public class NetworkClient : NetworkBase
+    {
+
+        private NetClient NetClientObject { get { return (NetClient)NetPeerObject; } }
+        public string HostAddress { get; set; }
+        private bool _isConnectedToServer;
+        public bool IsConnectedToServer { get { return _isConnectedToServer; } set { _isConnectedToServer = value; OnPropertyChanged(); } }
+        private bool _istConnectedToGame;
+        public bool IsConnectedToGame { get { return _istConnectedToGame; } private set { _istConnectedToGame = value; OnPropertyChanged(); } }
+        public Entity CurrentFaction { get; set; }
+
+        public string ConnectedToGameName { get; private set; }
+        public DateTime ConnectedToDateTime { get; private set; }
+        //private Dictionary<Guid, string> _factions; 
+        public ObservableCollection<FactionItem> Factions { get; set; }
+
+        public NetworkClient(string hostAddress, int portNum)
+        {
+   
+            PortNum = portNum;
+            HostAddress = hostAddress;
+            IsConnectedToServer = false;
+            Factions = new ObservableCollection<FactionItem>();
+        }
+
+        public void ClientConnect()
+        {
+            var config = new NetPeerConfiguration("Pulsar4X");
+            config.EnableMessageType(NetIncomingMessageType.DiscoveryResponse);
+            NetPeerObject = new NetClient(config);
+            NetPeerObject.Start();
+            NetClientObject.DiscoverLocalPeers(PortNum);
+            NetPeerObject.Connect(host: HostAddress, port: PortNum);
+            StartListning();
+
+        }
+
+
+        protected override void HandleDiscoveryResponce(NetIncomingMessage message)
+        {
+            ConnectedToGameName = message.ReadString();
+            ConnectedToDateTime = new DateTime(message.ReadInt64());
+            Messages.Add("Found Server: " + message.SenderEndPoint + "Name Is: " + ConnectedToGameName);
+        }
+
+        protected override void HandleGameDataMessage(NetIncomingMessage message)
+        {
+            ConnectedToGameName = message.ReadString();
+            ConnectedToDateTime = new DateTime(message.ReadInt64());
+        }
+
+
+        protected override void HandleTickInfo(NetIncomingMessage message)
+        {
+            ConnectedToDateTime = new DateTime(message.ReadInt64());
+            int delta = message.ReadInt32();
+            delta += (int)(ConnectedToDateTime - Game.CurrentDateTime).TotalSeconds;
+            Game.AdvanceTime(delta);
+        }
+
+        protected override void HandleEntityData(NetIncomingMessage message)
+        {
+
+            Guid entityID = new Guid(message.ReadBytes(16));
+            int len = message.ReadInt32();
+            byte[] data = message.ReadBytes(len);
+
+            if (Game == null || Game.GameName != ConnectedToGameName) //TODO handle if connecting to a game when in the middle of a singleplayer game. (ie prompt save)
+            {
+                Game = Game.NewGame(ConnectedToGameName, ConnectedToDateTime, 0, null, false);
+                
+            }
+
+            var mStream = new MemoryStream(data);
+            CurrentFaction = SaveGame.ImportEntity(Game, Game.GlobalManager, mStream);
+            IsConnectedToGame = true;
+        }
+
+        protected override void ConnectionStatusChanged(NetIncomingMessage message)
+        {
+            switch (message.SenderConnection.Status)
+            {
+                case NetConnectionStatus.Connected:
+                    IsConnectedToServer = true;
+                    break;
+                case NetConnectionStatus.Disconnected:
+                    IsConnectedToServer = false;
+                    IsConnectedToGame = false;
+                    break;
+            }
+        }
+
+
+        public void SendFactionDataRequest(string faction, string password)
+        {
+
+            NetOutgoingMessage sendMsg = NetPeerObject.CreateMessage();
+            sendMsg.Write((byte)DataMessageType.FactionDataRequest);
+            sendMsg.Write(faction);
+            sendMsg.Write(password);
+            Encrypt(sendMsg);//sequence channel 31 is expected to be encrypted by the recever. see NetworkBase GotMessage()
+            NetClientObject.SendMessage(sendMsg, NetClientObject.ServerConnection, NetDeliveryMethod.ReliableOrdered, SecureChannel);
+        }
+
+        //public void ReceveFactionList(DataMessage dataMessage)
+        //{
+        //    Factions.Clear();
+
+        //    foreach (var factionItem in (List<FactionItem>)dataMessage.DataObject)
+        //    {
+        //        Factions.Add(factionItem);
+        //    }
+        //}
+
+        //public void SendFactionListRequest()
+        //{
+        //    DataMessage dataMessage = new DataMessage();
+        //    dataMessage.DataMessageType = DataMessageType.FactionDictionary;
+
+        //    var binFormatter = new BinaryFormatter();
+        //    var mStream = new MemoryStream();
+        //    binFormatter.Serialize(mStream, dataMessage);
+
+
+        //    NetOutgoingMessage sendMsg = NetClientObject.CreateMessage();
+        //    sendMsg.Write(mStream.ToArray()); //send the stream as an byte array. 
+        //    NetClientObject.SendMessage(sendMsg, NetClientObject.ServerConnection, NetDeliveryMethod.ReliableOrdered);
+        //}
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
