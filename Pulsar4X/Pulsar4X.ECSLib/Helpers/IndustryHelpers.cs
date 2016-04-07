88ae2ddf25bf32d82834b55ac07403ab1df86d26
Rod Serling <ScottPhelps.usn@gmail.com>
2016-04-06 19:16:40 -0500

    Work on IndustryJob Serialization/Deserialization and construction

--- Pulsar4X/Pulsar4X.ECSLib/Helpers/IndustryHelpers.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Helpers/IndustryHelpers.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Runtime.Serialization;
 
 namespace Pulsar4X.ECSLib
 {
@@ -22,19 +23,31 @@ namespace Pulsar4X.ECSLib
         Refining                    = 1 << 10,
     }
 
+    [JsonObject(MemberSerialization.OptIn)]
     public class IndustryJob
     {
+        [JsonProperty]
         public Entity ProjectManager { get; internal set; }
+
+        [JsonProperty]
+        public Entity OwningFaction { get; internal set; }
 
         public IndustryType IndustryType { get; internal set; }
 
+        [JsonProperty]
         public Guid ItemGuid { get; internal set; }
+        public string ItemName { get; internal set; }
+        [JsonProperty]
         public int NumberOrdered { get; set; }
+        [JsonProperty]
         public int NumberCompleted { get; internal set; }
 
-        public float PartialBPApplied { get; internal set; }
         public float BPPerItem { get; internal set; }
+        [JsonProperty]
+        public float PartialBPApplied { get; internal set; }
+        public float BPToNextItem => BPPerItem - PartialBPApplied;
 
+        [JsonProperty]
         public float PercentToUtilize { get; set; }
 
         public float TotalBPApplied => NumberCompleted * BPPerItem + PartialBPApplied;
@@ -42,14 +55,67 @@ namespace Pulsar4X.ECSLib
         public float PercentCompleted => TotalBPApplied / TotalBPRequired;
 
         public DateTime ProjectedCompletion { get; internal set; }
-
-        [JsonProperty]
+        
         internal Dictionary<Guid, float> materialsRequiredPerItem { get; set; } = new Dictionary<Guid, float>();
-
-        [JsonIgnore]
+        
         public IReadOnlyDictionary<Guid, float> MaterialsRequiredPerItem => materialsRequiredPerItem;
 
+        [JsonProperty]
         public bool AutoRepeat { get; set; }
+
+        public IndustryJob(Game game, Entity owningFaction, Guid itemGuid, int numberOrdered, float percentToUtilize, bool autoRepeat = false)
+        {
+            OwningFaction = owningFaction;
+            ItemGuid = itemGuid;
+            NumberOrdered = numberOrdered;
+            PercentToUtilize = percentToUtilize;
+            AutoRepeat = autoRepeat;
+
+            SetupJob(game);
+        }
+
+        [OnDeserialized]
+        public void OnDeserialized(StreamingContext context)
+        {
+            SetupJob((Game)context.Context);
+        }
+
+        private void SetupJob(Game game)
+        {
+            object itemSD = game.StaticData.FindDataObjectUsingID(ItemGuid);
+            if (itemSD != null)
+            {
+                // Item is a static-data object. Resolve the type.
+                // Note to implementor: This will be required for Research at least.
+                throw new NotImplementedException();
+            }
+
+            var itemEntity = game.GlobalManager.GetLocalEntityByGuid(ItemGuid);
+
+            if (!itemEntity.IsValid)
+            {
+                throw new ArgumentException("Guid could not be resolved.", nameof(ItemGuid));
+            }
+
+            ItemName = itemEntity.GetDataBlob<NameDB>().GetName(OwningFaction);
+
+            var componentInfoDB = itemEntity.GetDataBlob<ComponentInfoDB>();
+            if (componentInfoDB == null)
+            {
+                throw new ArgumentException("Provided Guid resolved to an entity without a ComponentInfoDB.");
+            }
+
+            BPPerItem = componentInfoDB.BuildPointCost;
+            foreach (KeyValuePair<Guid, int> materialCost in componentInfoDB.MaterialCosts)
+            {
+                materialsRequiredPerItem.Add(materialCost.Key, materialCost.Value);
+            }
+
+            foreach (KeyValuePair<Guid, int> componentCost in componentInfoDB.ComponentCosts)
+            {
+                materialsRequiredPerItem.Add(componentCost.Key, componentCost.Value);
+            }
+        }
     }
 
     /// <summary>
