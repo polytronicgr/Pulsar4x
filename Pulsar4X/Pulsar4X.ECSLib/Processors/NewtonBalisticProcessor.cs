8488df000a1a7541c7514843130a3e3f1cf630cd
NathanH- <nhdump@gmail.com>
2016-12-17 20:58:24 -0600

    Updated the remove routine but it still crashes.

--- Pulsar4X/Pulsar4X.ECSLib/Processors/NewtonBalisticProcessor.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Processors/NewtonBalisticProcessor.cs
@@ -22,10 +22,13 @@ namespace Pulsar4X.ECSLib
             manager.ManagerSubpulses.AddSystemInterupt(toDate + orbitCycle, PulseActionEnum.BalisticMoveProcessor);
 
             List<Entity> RemoveList = new List<Entity>();
-            List<StarSystem> RemoveSystem = new List<StarSystem>();
+            //List<StarSystem> RemoveSystem = new List<StarSystem>();
 
+            int eCount = 0;
             foreach (Entity objectEntity in manager.GetAllEntitiesWithDataBlob<NewtonBalisticDB>())
             {
+                eCount++;
+
                 NewtonBalisticDB balisticDB = objectEntity.GetDataBlob<NewtonBalisticDB>();
                 PositionDB position = objectEntity.GetDataBlob<PositionDB>();
                 position.RelativePosition += Distance.KmToAU(balisticDB.CurrentSpeed * orbitCycle.TotalSeconds);
@@ -33,35 +36,44 @@ namespace Pulsar4X.ECSLib
                 Entity myTarget = manager.GetLocalEntityByGuid(balisticDB.TargetGuid);
                 PositionDB targetPos = myTarget.GetDataBlob<PositionDB>();
 
-                if(targetPos.AbsolutePosition == position.AbsolutePosition)
+                double xDist = targetPos.AbsolutePosition.X - position.AbsolutePosition.X;
+                double yDist = targetPos.AbsolutePosition.Y - position.AbsolutePosition.Y;
+                double dist = Math.Sqrt((xDist * xDist) + (yDist * yDist));
+                double kmDist = Distance.AuToKm(dist);
+                if (kmDist < 1000.0)
                 {
                     //do something in damage processor for asteroid hitting a planet?
                     DamageProcessor.OnTakingDamage(myTarget, 1000000); ///one. million. damage points.
 
-                    StarSystem mySystem;
-                    if (!manager.Game.Systems.TryGetValue(position.SystemGuid, out mySystem))
-                        throw new GuidNotFoundException(position.SystemGuid);
+                    //StarSystem mySystem;
+                    //if (!manager.Game.Systems.TryGetValue(position.SystemGuid, out mySystem))
+                    //    throw new GuidNotFoundException(position.SystemGuid);
 
-                    RemoveList.Add(objectEntity);
-                    RemoveSystem.Add(mySystem);
+                    if(!RemoveList.Contains(objectEntity))
+                        RemoveList.Add(objectEntity);
+                    //RemoveSystem.Add(mySystem);
 
-                    mySystem.SystemManager.RemoveEntity(objectEntity); //get rid of the asteroid
+                    //manager.RemoveEntity(objectEntity); //get rid of the asteroid
                 }
             }
 
             /// <summary>
             /// Clean up the asteroids that have hit something and been put in the remove list.
             /// </summary>
+            /// Asteroids aren't in the system manager. oops?
             for(int removeIterator = 0; removeIterator < RemoveList.Count; removeIterator++)
             {
-                RemoveSystem[removeIterator].SystemManager.RemoveEntity(RemoveList[removeIterator]);
+                //RemoveSystem[removeIterator].SystemManager.RemoveEntity(RemoveList[removeIterator]);
+                //Will destroy work?
+                RemoveList[removeIterator].Destroy();
+                //manager.RemoveEntity(RemoveList[removeIterator]);
             }
 
             /// <summary>
             /// This may not be necessary but clear these two lists.
             /// </summary>
             RemoveList.Clear();
-            RemoveSystem.Clear();
+            //RemoveSystem.Clear();
         }
     }
 }
