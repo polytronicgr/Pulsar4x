80d04c02d23fe539bd7b15072948f5a2cda5d911
Rod Serling <ScottPhelps.usn@gmail.com>
2017-07-09 20:21:40 -0500

    ReSharper Code Cleanup - Entity.cs

--- Pulsar4X/Pulsar4X.ECSLib/Entity/Entity.cs
+++ Pulsar4X/Pulsar4X.ECSLib/Entity/Entity.cs
@@ -1,9 +1,9 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using Newtonsoft.Json;
 
 namespace Pulsar4X.ECSLib
 {
@@ -12,9 +12,76 @@ namespace Pulsar4X.ECSLib
     [PublicAPI]
     public sealed class Entity : ProtoEntity
     {
+        #region Types
+        /// <summary>
+        /// EntityConverter is responsible for deserializing entities when they are encountered as references.
+        /// The EntityConverter must provide a proper reference to the object being deserialized.
+        /// </summary>
+        private class EntityConverter : JsonConverter
+        {
+            #region Interfaces, Overrides, and Operators
+            public override bool CanConvert(Type objectType) => objectType == typeof(Entity);
+
+            /// <summary>
+            /// Returns a Entity object that represents the entity.
+            /// If the Entity's manager has already deserialized the entity, then the EntityManager's reference is returned.
+            /// If not, then we create the entity in the global manager, and when the EntityManager containing this entity deserializes,
+            /// it will transfer the entity (that we create here) to itself. This will preserve all Entity references already deserialized.
+            /// </summary>
+            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+            {
+                var game = (Game)serializer.Context.Context;
+                Entity entity;
+
+                // Parse the Guid from the reader.
+                Guid entityGuid = Guid.Parse(reader.Value.ToString());
+
+                // Lookup the entity using a global Guid lookup.
+                if (entityGuid == Guid.Empty)
+                {
+                    return InvalidEntity;
+                }
+                if (game.GlobalManager.FindEntityByGuid(entityGuid, out entity))
+                {
+                    return entity;
+                }
+
+                // If no entity was found, create a new entity in the global manager.
+                entity = new Entity(game.GlobalManager, entityGuid);
+                return entity;
+            }
+
+            /// <summary>
+            /// Serializes the Entity objects. Entities are serialized as simple Guids in this method.
+            /// Datablobs are saved during EntityManager serialization.
+            /// </summary>
+            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+            {
+                var entity = (Entity)value;
+                serializer.Serialize(writer, entity.Guid);
+            }
+            #endregion
+        }
+        #endregion
+
+        #region Constants
+        private static readonly EntityManager InvalidManager = EntityManager.InvalidManager;
+
+        /// <summary>
+        /// Static entity reference to an invalid entity.
+        /// Functions must never return a null entity. Instead, return InvalidEntity.
+        /// </summary>
+        [NotNull]
+        [PublicAPI]
+        public static readonly Entity InvalidEntity = new Entity();
+        #endregion
+
+        #region Fields
         // Index slot of this entity's datablobs in its EntityManager.
         internal int ID;
+        #endregion
 
+        #region Properties
         private string DebugDisplay => GetDataBlob<NameDB>()?.DefaultName ?? Guid.ToString();
 
         [NotNull]
@@ -24,24 +91,9 @@ namespace Pulsar4X.ECSLib
         [NotNull]
         [PublicAPI]
         public new ReadOnlyCollection<BaseDataBlob> DataBlobs => IsValid ? new ReadOnlyCollection<BaseDataBlob>(Manager.GetAllDataBlobs(ID)) : new ReadOnlyCollection<BaseDataBlob>(new List<BaseDataBlob>());
+        #endregion
 
-        private static readonly EntityManager InvalidManager = EntityManager.InvalidManager;
-
-        /// <summary>
-        /// Static entity reference to an invalid entity.
-        /// 
-        /// Functions must never return a null entity. Instead, return InvalidEntity.
-        /// </summary>
-        [NotNull]
-        [PublicAPI]
-        public static readonly Entity InvalidEntity = new Entity();
-
-        #region Entity Constructors
-        private Entity()
-        {
-            Manager = InvalidManager;
-        }
-
+        #region Constructors
         internal Entity([NotNull] EntityManager manager, IEnumerable<BaseDataBlob> dataBlobs = null) : this(manager, Guid.NewGuid(), dataBlobs) { }
 
         internal Entity([NotNull] EntityManager manager, Guid id, IEnumerable<BaseDataBlob> dataBlobs = null)
@@ -50,7 +102,8 @@ namespace Pulsar4X.ECSLib
             Guid = id;
 
             Entity checkEntity;
-            while (Guid == Guid.Empty || manager.FindEntityByGuid(Guid, out checkEntity))
+            while (Guid == Guid.Empty ||
+                   manager.FindEntityByGuid(Guid, out checkEntity))
             {
                 Guid = Guid.NewGuid();
             }
@@ -73,12 +126,55 @@ namespace Pulsar4X.ECSLib
         }
 
         internal Entity([NotNull] EntityManager manager, [NotNull] ProtoEntity protoEntity) : this(manager, protoEntity.Guid, protoEntity.DataBlobs) { }
+
+        private Entity() { Manager = InvalidManager; }
+        #endregion
+
+        #region Interfaces, Overrides, and Operators
+        /// <summary>
+        /// Simple override to display entities as their Guid.
+        /// Used mostly in debugging.
+        /// </summary>
+        public override string ToString() => Guid.ToString();
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Used to transfer an entity between managers.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Thrown when trying to transfer the static InvalidEntity.</exception>
+        /// <exception cref="ArgumentNullException">Thrown when the provided manager is null.</exception>
+        public void Transfer([NotNull] EntityManager newManager)
+        {
+            if (!IsValid)
+            {
+                throw new InvalidOperationException("Cannot transfer an invalid entity.");
+            }
+            if (newManager == null)
+            {
+                throw new ArgumentNullException(nameof(newManager));
+            }
+            // Store dataBlobs.
+            List<BaseDataBlob> dataBlobs = Manager.GetAllDataBlobs(ID);
+
+            // Remove myself from the old manager.
+            Manager.RemoveEntity(this);
+
+            // Add myself the the new manager.
+            ID = newManager.SetupEntity(this);
+            Manager = newManager;
+            _protectedDataBlobMask_ = Manager.EntityMasks[ID];
+
+            foreach (BaseDataBlob dataBlob in dataBlobs)
+            {
+                SetDataBlob(dataBlob);
+            }
+        }
         #endregion
 
         #region Public API Functions
         /// <summary>
         /// Used to determine if an entity is valid.
-        /// 
         /// Entities are considered valid if they are not the static InvalidEntity and are properly registered to a manager.
         /// </summary>
         [PublicAPI]
@@ -98,10 +194,7 @@ namespace Pulsar4X.ECSLib
             return new Entity(manager, dataBlobs);
         }
 
-        public static Entity Create(EntityManager manager, ProtoEntity protoEntity)
-        {
-            return new Entity(manager, protoEntity.Guid, protoEntity.DataBlobs);
-        }
+        public static Entity Create(EntityManager manager, ProtoEntity protoEntity) => new Entity(manager, protoEntity.Guid, protoEntity.DataBlobs);
 
         [PublicAPI]
         public void Destroy()
@@ -215,7 +308,7 @@ namespace Pulsar4X.ECSLib
             if (!IsValid)
             {
                 throw new InvalidOperationException("Cannot remove a datablob from an invalid entity.");
-            } 
+            }
             if (!HasDataBlob(typeIndex))
             {
                 throw new InvalidOperationException("Entity does not contain this datablob.");
@@ -229,7 +322,8 @@ namespace Pulsar4X.ECSLib
         /// <typeparam name="T">Type of datablob to check for.</typeparam>
         /// <returns>True if the entity has the datablob.</returns>
         [PublicAPI]
-        public bool HasDataBlob<T>() where T : BaseDataBlob
+        public bool HasDataBlob<T>()
+            where T : BaseDataBlob
         {
             if (!IsValid)
             {
@@ -281,96 +375,6 @@ namespace Pulsar4X.ECSLib
             clone.Guid = Guid.NewGuid();
             return new Entity(manager, clone);
         }
-
         #endregion
-
-        /// <summary>
-        /// Simple override to display entities as their Guid.
-        /// 
-        /// Used mostly in debugging.
-        /// </summary>
-        public override string ToString()
-        {
-            return Guid.ToString();
-        }
-
-        /// <summary>
-        /// Used to transfer an entity between managers.
-        /// </summary>
-        /// <exception cref="InvalidOperationException">Thrown when trying to transfer the static InvalidEntity.</exception>
-        /// <exception cref="ArgumentNullException">Thrown when the provided manager is null.</exception>
-        public void Transfer([NotNull] EntityManager newManager)
-        {
-            if (!IsValid)
-            {
-                throw new InvalidOperationException("Cannot transfer an invalid entity.");
-            }
-            if (newManager == null)
-            {
-                throw new ArgumentNullException(nameof(newManager));
-            }
-            // Store dataBlobs.
-            List<BaseDataBlob> dataBlobs = Manager.GetAllDataBlobs(ID);
-
-            // Remove myself from the old manager.
-            Manager.RemoveEntity(this);
-
-            // Add myself the the new manager.
-            ID = newManager.SetupEntity(this);
-            Manager = newManager;
-            _protectedDataBlobMask_ = Manager.EntityMasks[ID];
-
-            foreach (BaseDataBlob dataBlob in dataBlobs)
-            {
-                SetDataBlob(dataBlob);
-            }
-        }
-
-        /// <summary>
-        /// EntityConverter is responsible for deserializing entities when they are encountered as references. 
-        /// The EntityConverter must provide a proper reference to the object being deserialized.
-        /// </summary>
-        private class EntityConverter : JsonConverter
-        {
-            public override bool CanConvert(Type objectType)
-            {
-                return objectType == typeof(Entity);
-            }
-
-            /// <summary>
-            /// Returns a Entity object that represents the entity.
-            /// If the Entity's manager has already deserialized the entity, then the EntityManager's reference is returned.
-            /// If not, then we create the entity in the global manager, and when the EntityManager containing this entity deserializes,
-            /// it will transfer the entity (that we create here) to itself. This will preserve all Entity references already deserialized.
-            /// </summary>
-            public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
-            {
-                Game game = (Game)serializer.Context.Context;
-                Entity entity;
-
-                // Parse the Guid from the reader.
-                Guid entityGuid = Guid.Parse(reader.Value.ToString());
-
-                // Lookup the entity using a global Guid lookup.
-                if (entityGuid == Guid.Empty)
-                    return InvalidEntity;
-                if (game.GlobalManager.FindEntityByGuid(entityGuid, out entity))
-                    return entity;
-
-                // If no entity was found, create a new entity in the global manager.
-                entity = new Entity(game.GlobalManager, entityGuid);
-                return entity;
-            }
-
-            /// <summary>
-            /// Serializes the Entity objects. Entities are serialized as simple Guids in this method.
-            /// Datablobs are saved during EntityManager serialization.
-            /// </summary>
-            public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-            {
-                var entity = (Entity)value;
-                serializer.Serialize(writer, entity.Guid);
-            }
-        }
     }
 }
